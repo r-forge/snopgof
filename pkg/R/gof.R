03d410205e16885ec2afd9affa499428f6fe61af
jalospinoso (Josh Lospinoso) <jalospinoso@gmail.com>
2011-01-26 00:40:46 +0000

    Spruced up plots, univariate distributions now work.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/snopgof@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gof.R
+++ pkg/R/gof.R
@@ -22,7 +22,9 @@ function(observed, simulated, weights=NULL) {
 	}
 	
 	foldedObservations <- sapply(1:observations, function(i) abs(observed[i,]-simulated$Expectation))
-	p <- .Call("doTest", foldedObservations, simulated$FoldedSimulations, simulated$CenteredComparisons, simulated$ComparisonCrease, weights)
+	p <- .Call("doTest", as.matrix(foldedObservations), as.matrix(simulated$FoldedSimulations),
+				as.matrix(simulated$CenteredComparisons), as.matrix(simulated$ComparisonCrease), 
+								as.matrix(weights))
 	
 	#	Comparisons in R instead of C
 	#	tailComparisons.observed = sapply(1:observations, function (i) gof.compare(abs(observed[i,]-simulated$Expectation), simulated$FoldedSimulations) )
@@ -43,10 +45,75 @@ print.gof.result <- function (x, ...) {
 	with(x, cat("snopgof results\nVariates:", Preprocess$Variates ,"\nSimulations:", Preprocess$Iterations ,"\nP-Value:", p,"\nP-Value(MHD):", p.MHD))
 }
 
-plot.gof.result <- function (x, ...) {
-	boxplot(as.numeric(x$Preprocess$Original)~rep(1:x$Preprocess$Variates, each=x$Preprocess$Iterations), ...)
-	temp <- (1:x$Preprocess$Variates)
-	for(i in 1:nrow(x$Observation)) {
-		lines(x$Observation[i,]~temp, col="red", type="l")
+plot.gof.result <- function (x, standardize=3, violin=TRUE, 
+		main="SNOPGOF Goodness of Fit Test", ylim=NULL, xlim=NULL, 
+		xlab=NULL, ylab=NULL, key=NULL, perc=.05, ...) {
+	itns <- x$Preprocess$Iterations
+	vars <- x$Preprocess$Variates
+	sims <- x$Preprocess$Original
+	obs <- x$Observation
+	n.obs <- nrow(obs)
+	
+	if (standardize==3) {
+		sims.median <- apply(sims, 2, median)
+		sims.min <- apply(sims, 2, min)
+		sims.max <- apply(sims, 2, max)
+		sims <- sapply(1:ncol(sims), function(i) (sims[,i] - sims.median[i])/(sims.max[i] - sims.min[i] ) )
+		obs <- matrix(sapply(1:ncol(sims), function(i) (obs[,i] - sims.median[i])/(sims.max[i] - sims.min[i] ) ), nrow=n.obs )
+	} else if (standardize==2) {
+		sims.min <- apply(sims, 2, min)
+		sims.max <- apply(sims, 2, max)
+		sims <- sapply(1:ncol(sims), function(i) (sims[,i] - sims.min[i])/(sims.max[i] - sims.min[i] ) )
+		obs <- matrix(sapply(1:ncol(sims), function(i) (obs[,i] - sims.min[i])/(sims.max[i] - sims.min[i] ) ), nrow=n.obs )
+	} else if (standardize==1) {
+		sims.mean <- apply(sims, 2, mean)
+		sims <- sapply(1:ncol(sims), function(i) (sims[,i] - sims.mean[i]) )
+		obs <- matrix(sapply(1:ncol(sims), function(i) (obs[,i] - sims.mean[i]) ), nrow=n.obs )
 	}
+	
+	if (is.null(ylim)) {
+		ylim = c(min(obs, sims), max(obs, sims))
+	}
+	if (is.null(xlim)) {
+		xlim = c(0, ncol(obs)+1)
+	}
+	if (is.null(xlab)) {
+		xlab= paste( paste("p:", round(x$p, 3), collapse = " "), paste("p(MHD):", round(x$p.MHD, 3), collapse = " "), collapse = "\n")
+	}
+	if (is.null(ylab)) {
+		ylab = "Statistic Values"
+	}
+	xAxis <- (1:vars)
+
+	plot(obs[1,]~xAxis, col="white", type="p",  ylim=ylim, xlim=xlim, main=main, xlab=xlab, ylab=ylab, axes=FALSE, ...)
+	if (!is.null(key)) {
+		if (length(key) != ncol(obs)) {
+			stop("Key length does not match the number of variates.")
+		}
+		axis(1, at=xAxis, lab=key)
+	} else {
+		axis(1, at=xAxis, lab=paste("v", xAxis, sep=""))
+	}
+
+	ind.lower = round(itns * perc/2)
+	ind.upper = round(itns * (1-perc/2))
+	yperc.lower = sapply(1:ncol(sims), function(i)  sort(sims[,i])[ind.lower]  )
+	yperc.upper = sapply(1:ncol(sims), function(i)  sort(sims[,i])[ind.upper]  )
+	lines(yperc.lower~xAxis, lty=3, col = "gray", lwd=3)
+	lines(yperc.upper~xAxis, lty=3, col = "gray", lwd=3)
+
+	if (violin) {
+		require(vioplot)
+		for (i in 1:ncol(sims)) {
+			vioplot(sims[,i], at=xAxis[i], add=TRUE, col="gray", wex=.75, ...)
+		}
+	} else {
+		boxplot(as.numeric(sim)~rep(1:vars, each=itns), add=TRUE, ...)
+	}
+	for(i in 1:nrow(obs)) {
+		lines(obs[i,]~xAxis, col="red", type="l", lwd=1, ...)
+		lines(obs[i,]~xAxis, col="red", type="p", lwd=3, pch=19, ...)
+		text(xAxis, obs[i,], labels=round(x$Observation[i,],3), pos=4)
+	}
+
 }
--- pkg/R/gof.preprocess.R
+++ pkg/R/gof.preprocess.R
@@ -6,10 +6,15 @@ function(simulated, creasingFunction=median) {
 	a <- cov(simulated)
 	ainv <- solve(a)
 	simulations=nrow(simulated)
+	variates=ncol(simulated)
 	expectation = apply(simulated, 2, creasingFunction);
 	centeredSimulations <- t(sapply(1:simulations, function(i) simulated[i,] - expectation))
+	if (variates==1) {
+		centeredSimulations <- t(centeredSimulations)
+	}
 	foldedSimulations = abs(centeredSimulations)
 	ttc <- system.time((tailComparisons.simulated <- .Call("comparePreprocess", foldedSimulations)))
+
 	comparisonCrease = apply(tailComparisons.simulated, 1, creasingFunction)
 	tailComparisons.simulated.centered = sapply(1:simulations, function (i) tailComparisons.simulated[,i] - comparisonCrease)
 	# Only use if C is broken:
